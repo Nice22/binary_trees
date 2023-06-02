110626cc82f91671328fc48f475f566a9206b556
Nice22 <benothouessou74@gmail.com>
2023-06-02 14:20:03 -0700

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,6 +8,7 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
@@ -23,3 +24,4 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	return (new);
 }
+
